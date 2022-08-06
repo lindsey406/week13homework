9fbe7f783ac722752201477b4b69f24b7112f791
Lindsey Brandt-Bennett <f55m818@CH104D-16242.local>
2022-08-05 21:24:14 -0600

    hw

---
+++ jeep-sales/src/main/java/com/promineotech/jeep/entity/JeepModel.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.entity;
+
+/**
+ * @author f55m818
+ *
+ */
+public enum JeepModel {
+
+  WRANGLER, GRAND_CHEROKEE, CHEROKEE, COMPASS, RENEGADE, GLADIATOR, WRANGLER_4XE
+}
